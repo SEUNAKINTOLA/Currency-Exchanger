a47de35b661fc01c9ed2eeff7a83a3b5161cbe39
Oluwaseun Akintola <akinsehun@gmail.com>
2024-02-20 01:44:16 +0100

    Implement angular environment

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
-  private apiUrl = 'http://data.fixer.io/api/latest';
-  private apiKey = '3f1f2a44ed9e0f0cb71c38c25d983f27';//'8e63ce13dea7d40e2e276a297009f199';
+  private apiUrl = environment.fixerApiBaseUrl;
+  private apiKey = environment.apiKey;
 
   constructor(private http: HttpClient) {}
 
   getCurrencies(): Observable<any> {
-    const url = `${this.apiUrl}?access_key=${this.apiKey}`;
+    const url = `${this.apiUrl}/latest?access_key=${this.apiKey}`;
     return this.http.get<any>(url);
   }
 
   convertCurrency(convertFrom: string, convertTo: string): Observable<any> {
-    const url = `${this.apiUrl}?access_key=${this.apiKey}&base=${convertFrom}&symbols=${convertTo}`;
+    const url = `${this.apiUrl}/latest?access_key=${this.apiKey}&base=${convertFrom}&symbols=${convertTo}`;
     return this.http.get<any>(url);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiKey: '3f1f2a44ed9e0f0cb71c38c25d983f27',
+  fixerApiBaseUrl: 'http://data.fixer.io/api'
+};
